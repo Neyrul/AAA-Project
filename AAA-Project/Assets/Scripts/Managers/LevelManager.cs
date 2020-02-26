da677be9b01c1edf1010fdc6625ac7d8afee885d
papazian julien <julien_papazian@hotmail.fr>
2020-02-27 00:51:54 +0100

    LevelManager & Village scene

---
+++ AAA-Project/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : Singleton<LevelManager>
+{
+	internal System.Action Callback_OnLevelWasLoaded = null;
+
+	void Start ()
+	{
+		SceneManager.sceneLoaded += LevelWasLoaded;
+	}
+
+	internal void LoadLevel (int levelToLoad)
+	{
+		System.GC.Collect();
+		SceneManager.LoadScene(levelToLoad);
+	}
+
+	internal void LoadLevel (string levelToLoad)
+	{
+		System.GC.Collect();
+		SceneManager.LoadScene(levelToLoad);
+	}
+
+	internal void LoadMenu ()
+	{
+		System.GC.Collect();
+		LoadLevel(0);
+	}
+
+	internal void LoadNextLevel ()
+	{
+		int activeSceneCount = SceneManager.GetActiveScene().buildIndex;
+
+		if (activeSceneCount != SceneManager.sceneCountInBuildSettings - 1)
+			LoadLevel(activeSceneCount + 1);
+
+		else
+			LoadMenu(); // ou load l'écran de fin
+	}
+
+	void LevelWasLoaded (Scene scene, LoadSceneMode mode)
+	{
+		if (scene.buildIndex != 0)
+			Callback_OnLevelWasLoaded?.Invoke();
+	}
+}
