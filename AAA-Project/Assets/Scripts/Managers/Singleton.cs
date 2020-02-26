97ca92f20b202d13255039403c07694d04967b9b
papazian julien <julien_papazian@hotmail.fr>
2020-02-26 22:44:30 +0100

    Singleton architecture

---
+++ AAA-Project/Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    internal static T Instance { get; private set; }
+
+    protected virtual void Awake ()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+
+        Instance = this as T;
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
